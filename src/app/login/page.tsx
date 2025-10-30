507acc5f9f2748189d46a8804cc55a2bb194d204
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-10-30 19:24:17 +0300

    feat: login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,19 @@
+import LoginForm from '@/components/login-form';
+import Link from 'next/link';
+
+export default function LoginPage() {
+  return (
+    <main className="md:max-w-lg h-screen mx-auto flex flex-col justify-center p-6 gap-5">
+      <div>
+        <h1 className="text-2xl font-bold leading-10">Login</h1>
+        <div className="flex gap-2">
+          <p>Don&apos;t have an account?</p>
+          <Link href="/register" className="text-blue-500">
+            Register
+          </Link>
+        </div>
+      </div>
+      <LoginForm />
+    </main>
+  );
+}
