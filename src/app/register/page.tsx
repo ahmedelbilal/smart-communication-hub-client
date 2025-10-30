08b1aa5f931fdab94d122e2b67a38740a5bde6d9
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-10-30 19:24:35 +0300

    feat: register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,19 @@
+import RegisterForm from '@/components/register-form';
+import Link from 'next/link';
+
+export default function RegisterPage() {
+  return (
+    <main className="md:max-w-lg h-screen mx-auto flex flex-col justify-center p-6 gap-5">
+      <div>
+        <h1 className="text-2xl font-bold">Register</h1>
+        <div className="flex gap-2">
+          <p>Already have and account?</p>
+          <Link href="/login" className="text-blue-500">
+            Login
+          </Link>
+        </div>
+      </div>
+      <RegisterForm />
+    </main>
+  );
+}
