c043250c0f601fd8ffc1561d8f5a19e58a68d0b9
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-10-31 22:25:42 +0200

    fix(EXTERNAL_API_BASE): default url

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const EXTERNAL_API_BASE = process.env.EXTERNAL_API_BASE || 'http://localhost:3000';
+export const EXTERNAL_API_BASE = process.env.EXTERNAL_API_BASE || 'http://localhost:3000/api';
 export const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3000';
