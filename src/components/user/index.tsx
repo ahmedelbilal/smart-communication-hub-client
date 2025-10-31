a376e384e1fe8ed9589b6244b7cda297d95ee486
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-10-31 22:34:14 +0200

    feat: user component

---
+++ src/components/user/index.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@/lib/utils';
+import { User as UserType } from '@/types/user';
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+import Avatar from '../avatar';
+
+export type UserProps = { user: UserType } & DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>;
+
+const User: React.FC<UserProps> = ({ user, className, ...props }) => {
+  return (
+    <div
+      className={cn('w-full hover:bg-gray-100 p-2 rounded-xl flex items-center gap-2', className)}
+      {...props}
+    >
+      <div className="relative">
+        <Avatar className="bg-amber-300 md:w-10 md:h-10" text={user.name} />
+      </div>
+      <span className="first-letter:uppercase collapsable">{user.name}</span>
+    </div>
+  );
+};
+
+export default User;
